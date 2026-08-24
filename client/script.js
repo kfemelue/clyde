aa98c32ee46667c47443a3e486a4e8f1fc0ce306
kfemelue <kfemelue@gmail.com>
2026-08-24 13:10:45 -0500

    Add an HTML google AD placeholder with styling, add a "buy me coffee" button to header in html and add styling to it

--- client/script.js
+++ client/script.js
@@ -1,58 +1,60 @@
 async function askAssistant() {
+  await displayWaitingMessage();
 
-    await displayWaitingMessage();
+  const my_prompt = document.getElementById("user-input").value;
 
-    const my_prompt = document.getElementById('user-input').value;
+  const body = {
+    role: "user",
+    content: my_prompt,
+  };
+  const client = "https://clyde-server.onrender.com/ask"; // endpoint from api server
+  const response = await fetch(client, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+  const conversation = await response.json();
 
-    const body = { 
-        "role" : "user",
-        "content" : my_prompt
+  await hideWaitingMessage();
+  document.getElementById("user-input").value = "";
+
+  const parentNode = document.getElementById("chat-messages");
+
+  while (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
+
+  conversation.forEach((message) => {
+    if (message.role !== "system") {
+      let messageContainer = document.createElement("div");
+      let messageRole = document.createElement("h3");
+      let messageContent = document.createElement("p");
+
+      messageContent.id = "message-text";
+      messageRole.id = "message-role";
+
+      if (message.role == "assistant") {
+        messageContainer.id = "message-bot";
+        messageRole.innerText = "Clyde";
+      } else if (message.role == "user") {
+        messageContainer.id = "message-user";
+        messageRole.innerText = "You";
+      }
+
+      messageContent.innerText = message.content;
+
+      messageContainer.appendChild(messageRole);
+      messageContainer.appendChild(messageContent);
+      parentNode.appendChild(messageContainer);
     }
-    const client = "https://clyde-server.onrender.com/ask" // endpoint from api server
-    const response = await fetch(client, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body) })
-    const conversation = await response.json()
-
-    await hideWaitingMessage();
-    document.getElementById('user-input').value="";
-
-    const parentNode = document.getElementById("chat-messages");
-
-    while (parentNode.firstChild) {
-        parentNode.removeChild(parentNode.firstChild);
-    }
-
-    conversation.forEach(message => {
-        if (message.role !== "system") {
-            let messageContainer = document.createElement('div');
-            let messageRole = document.createElement('h3');
-            let messageContent = document.createElement('p');
-
-            messageContent.id = "message-text";
-            messageRole.id = "message-role";
-
-            if (message.role == "assistant") {
-                messageContainer.id = "message-bot";
-                messageRole.innerText="Clyde";
-            } else if (message.role == "user") {
-                messageContainer.id = "message-user";
-                messageRole.innerText="You";
-            }
-
-            messageContent.innerText=message.content;
-            
-
-            messageContainer.appendChild(messageRole);
-            messageContainer.appendChild(messageContent);
-            parentNode.appendChild(messageContainer);
-        }
-    })
+  });
 }
 
-
 function hideWaitingMessage() {
-    document.getElementById("awaiting-response-message").className = "display-none";
+  document.getElementById("awaiting-response-message").className =
+    "display-none";
 }
 
 function displayWaitingMessage() {
-    document.getElementById("awaiting-response-message").className = "";
+  document.getElementById("awaiting-response-message").className = "";
 }
